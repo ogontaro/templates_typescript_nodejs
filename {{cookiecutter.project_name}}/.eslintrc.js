8a62a945fc5e8e03db71bc113017a045306690d6
ogontaro <ogontaro@gmail.com>
2023-12-27 23:45:15 +0900

    update

--- {{cookiecutter.project_name}}/.eslintrc.js
+++ {{cookiecutter.project_name}}/.eslintrc.js
@@ -1,23 +1,20 @@
 module.exports = {
-  parser: '@typescript-eslint/parser', // TypeScriptのパーサーを指定
-  plugins: ['@typescript-eslint'], // TypeScriptプラグインを使用
+  parser: "@typescript-eslint/parser", // TypeScriptのパーサーを指定
+  plugins: ["@typescript-eslint"], // TypeScriptプラグインを使用
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended' // TypeScriptの推奨ルールを使用
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended", // TypeScriptの推奨ルールを使用
+    "plugin:prettier/recommended", // Prettierの推奨設定を使用
   ],
   env: {
     node: true, // Node.jsのグローバル変数とスコープを有効にする
-    es2021: true // ES2021の構文を有効にする
+    es2021: true, // ES2021の構文を有効にする
   },
   parserOptions: {
     ecmaVersion: 2021, // ES2021を指定
-    sourceType: 'module' // モジュールシステムを使用
+    sourceType: "module", // モジュールシステムを使用
   },
   rules: {
     // 追加のルールやオーバーライドするルールをここに記述
   },
-  extends: [
-    // 他のextendsに加えて
-    'plugin:prettier/recommended' // Prettierの推奨設定を使用
-  ]
 };
--- {{cookiecutter.project_name}}/src/app.ts
+++ {{cookiecutter.project_name}}/src/app.ts
@@ -1,1 +1,3 @@
-console.log('Hello World')
+import "dotenv/config";
+
+console.log("Hello World");
